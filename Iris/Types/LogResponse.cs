7887a93b97e74c4d129930990d59d3b0c0f377a7
Puxxalwl <puxxalwl98@gmail.com>
2025-09-13 14:40:20 +0300

    Добавлен тип для получения логов

---
+++ Iris/Types/LogResponse.cs
@@ -0,0 +1,24 @@
+using System.Text.Json.Serialization;
+
+namespace Iris.Types;
+
+public class UpdateLog
+{
+    // Ид события
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    // Тип события: sweets_log — логи ирисок, gold_log — логи ирис-голд
+    [JsonPropertyName("type")]
+    public string Type { get; set; } = string.Empty;
+
+    // Время события
+    [JsonPropertyName("date")]
+    public int Date { get; set; }
+
+    // Объект собитыя
+    [JsonPropertyName("object")]
+    public Transaction Obj { get; set; } = null!;
+
+
+}
