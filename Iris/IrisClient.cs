fbdbace921199e7d98aaf8a79664ccda920cf183
Puxxalwl <puxxalwl98@gmail.com>
2025-09-13 14:41:25 +0300

    Создана оболочка для будущего клиента

---
+++ Iris/IrisClient.cs
@@ -0,0 +1,8 @@
+namespace Iris;
+
+public class IrisClient
+{
+    private static readonly string _irisVersion = "0.2";
+    private static readonly string _irisUrl = $"https://iris-tg.ru/api/v{_irisVersion}/";
+
+}
