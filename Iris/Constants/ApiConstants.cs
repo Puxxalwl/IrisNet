f73acc81bf73d36b697c1a09c1ad801786800135
Puxxalwl <puxxalwl98@gmail.com>
2025-09-13 14:31:38 +0300

    Созданы константы с путями к методам

---
+++ Iris/Constants/ApiConstants.cs
@@ -0,0 +1,18 @@
+namespace Iris.Constants;
+
+public static class ApiConstants
+{
+    public const string GiveSweets = "pocket/sweets/give";
+    public const string Balance = "pocket/balance";
+    public const string SweetsHistory = "pocket/sweets/history";
+    public const string GiveGold = "pocket/gold/give";
+    public const string GoldHistory = "pocket/gold/history";
+    public const string Enable = "pocket/enable";
+    public const string Disable = "pocket/disable";
+    public const string AllowUser = "pocket/allow_user";
+    public const string DenyUser = "pocket/deny_user";
+    public const string AllAllow = "pocket/allow_all";
+    public const string AllDeny = "pocket/deny_all"; 
+    public const string GetUpdates = "get_updates";
+    public const string IrisAgents = "iris_agents";
+}
