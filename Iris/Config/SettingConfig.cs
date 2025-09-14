031c9d108855fe7b0cc13990336e8b2f5fcf22b5
Puxxalwl <puxxalwl98@gmail.com>
2025-09-14 13:08:48 +0300

    Добавлена проверка на null

--- Iris/Config/SettingConfig.cs
+++ Iris/Config/SettingConfig.cs
@@ -2,19 +2,24 @@ using DotNetEnv;
 
 namespace Iris.Config;
 
-public partial class IrisConfig
+internal partial class IrisConfig
 {
     public static ApiConfig Load()
     {
         Env.Load();
 
+        string BotId = Environment.GetEnvironmentVariable("BOT_ID")!;
+        string IrisToken = Environment.GetEnvironmentVariable("IRIS_TOKEN")!;
+
+        if (BotId == null || IrisToken == null) throw new ArgumentException("Отсутсвуют параметры BOT_ID, IRIS_TOKEN в .env");
+
         return new ApiConfig
         {
-            BotId = Environment.GetEnvironmentVariable("BOT_ID")!,
-            IrisToken = Environment.GetEnvironmentVariable("IRIS_TOKEN")!
+            BotId = BotId,
+            IrisToken = IrisToken
         };
     }
-    public class ApiConfig
+    internal class ApiConfig
     {
         public string BotId { get; set; } = string.Empty;
         public string IrisToken { get; set; } = string.Empty;
