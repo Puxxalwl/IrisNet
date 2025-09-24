5988bab1dabe4e32da7a5c77010cdd54fda71b7b
Puxxalwl <puxxalwl98@gmail.com>
2025-09-24 09:20:03 +0300

    Добавлены новые методы

--- Iris/IrisClient.cs
+++ Iris/IrisClient.cs
@@ -1,5 +1,6 @@
 using Iris.Config;
 using Iris.Constants;
+using Iris.Enums;
 using Iris.Helpers;
 using Iris.Types;
 
@@ -9,10 +10,13 @@ public class IrisClient
 {
     private readonly HttpClient _httpClient = new HttpClient();
     private readonly string _baseIrisUrl;
+    private readonly string BotId;
+    public static readonly string IrisLink = "https://t.me/iris_black_bot";
 
-    public IrisClient(string BotId, string IrisToken)
+    public IrisClient(string botId, string IrisToken)
     {
         _baseIrisUrl = $"https://iris-tg.ru/api/v{ApiConstants._versionIris}/{BotId}:{IrisToken}";
+        BotId = botId;
     }
 
     public static IrisClient FromEnv()
@@ -45,7 +49,7 @@ public class IrisClient
         try
         {
             if (Sweets <= 0) throw new InvalidOperationException("Число ирисок не можеть быть меньше 1");
-            if (Comment != "" && Comment.Length > 128) throw new ArgumentException("Длина комментария не может перевышать 128 символов");
+            if (!string.IsNullOrEmpty(Comment) && Comment.Length > 128) throw new ArgumentException("Длина комментария не может перевышать 128 символов");
 
             string url = $"{_baseIrisUrl}/{ApiConstants.GiveSweets}";
 
@@ -66,6 +70,34 @@ public class IrisClient
         }
     }
 
+    public async Task<GiveResult> GiveDonateScoreAsync(
+        long UserId,
+        double DonateScore,
+        string Comment = ""
+    )
+    {
+        try
+        {
+            if (DonateScore <= 0) throw new InvalidOperationException("Число ирисок не можеть быть меньше 1");
+            if (!string.IsNullOrEmpty(Comment) && Comment.Length > 128) throw new ArgumentException("Длина комментария не может перевышать 128 символов");
+
+            string url = $"{_baseIrisUrl}/{ApiConstants.DonateScoreGive}";
+
+            var Params = new Dictionary<string, object>
+            {
+                ["sweets"] = DonateScore,
+                ["user_id"] = UserId,
+                ["comment"] = Comment,
+            };
+
+            return await _httpClient.GetWithRetry<GiveResult>(url, Params);
+        }
+        catch (Exception err)
+        {
+            Logger.Error($"Ошибка перевода очков доната (ID: {UserId})", err);
+            throw;
+        }
+    }
     public async Task<GiveResult> GiveIrisGoldAsync(
         long UserId,
         double IrisGold,
@@ -190,6 +222,25 @@ public class IrisClient
             throw;
         }
     }
+
+    public async Task<Transaction[]> GetDonateScoreHistoryAsync(long Offset = 0, int Limit = 0)
+    {
+        try
+        {
+            string url = $"{_baseIrisUrl}/{ApiConstants.DonateScoreHistory}";
+            var Params = new Dictionary<string, object>
+            {
+                ["offset"] = Offset,
+                ["limit"] = Limit
+            };
+            return await _httpClient.GetWithRetry<Transaction[]>(url, Params);
+        }
+        catch (Exception err)
+        {
+            Logger.Error("Ошибка получения истории очков доната", err);
+            throw;
+        }
+    }
     public async Task<long[]> GetAgentsAsync()
     {
         try
@@ -229,6 +280,129 @@ public class IrisClient
         public async Task<Trades> GetOrderBookAsync()
         {
             return await _httpClient.GetWithRetry<Trades>(OrderBookUrl);
+        }
+    }
+
+    public string GeneratePermissionsDeepLink(IEnumerable<BotPermissionOptions> options, long? botIdForLink)
+    {
+        var botId = botIdForLink ?? long.Parse(BotId);
+        var link = $"{IrisLink}?start=request_rights_{botId}";
+
+        foreach (var opt in options)
+        {
+            link += $"_{opt}";
+        }
+        return link;
+    }
+
+
+    public string GenerateDeepLink(CurrenciesOptions currency, int count, string? comment, long? botIdForLink)
+    {
+        if (count <= 0) { throw new InvalidOperationException("Число не может быть меньше 1"); }
+        if (!string.IsNullOrEmpty(comment) && comment.Length > 128) { throw new ArgumentException("Длина комментария не может перевышать 128 символов"); }
+
+        long botId = botIdForLink ?? long.Parse(BotId);
+
+
+        string link = currency switch
+        {
+            CurrenciesOptions.Gold => $"{IrisLink}?start=givegold_bot{botId}_{count}",
+            CurrenciesOptions.Sweets => $"{IrisLink}?start=give_bot{botId}_{count}",
+            CurrenciesOptions.DonateScore => $"{IrisLink}?start=givedonate_score_bot{botId}_{count}",
+            _ => throw new ArgumentOutOfRangeException(nameof(currency), currency, null)
+        };
+        if (string.IsNullOrEmpty(comment)) link += $"_{comment}";
+        return link;
+    }
+
+    public async Task<ResultReg> GetUserRegAsync(long UserId)
+    {
+        try
+        {
+            string url = $"{_baseIrisUrl}/{ApiConstants.UserReg}";
+            var Params = new Dictionary<string, object>
+            {
+                ["user_id"] = UserId
+            };
+
+            return await _httpClient.GetWithRetry<ResultReg>(url, Params);
+        }
+        catch (Exception err)
+        {
+            Logger.Error("Не удалось получить регистрации пользователя", err);
+            throw;
+        }
+    }
+
+    public async Task<ResultSpam> GetUserSpamAsync(long UserId)
+    {
+        try
+        {
+            string url = $"{_baseIrisUrl}/{ApiConstants.UserSpam}";
+            var Params = new Dictionary<string, object>
+            {
+                ["user_id"] = UserId
+            };
+            return await _httpClient.GetWithRetry<ResultSpam>(url, Params);
+        }
+        catch (Exception err)
+        {
+            Logger.Error("Не удалось получить информацию о наказаниях пользователя", err);
+            throw;
+        }
+    }
+
+    public async Task<ResultActivity> GetUserActivityAsync(long UserId)
+    {
+        try
+        {
+            string url = $"{_baseIrisUrl}/{ApiConstants.UserActivity}";
+            var Params = new Dictionary<string, object>
+            {
+                ["user_id"] = UserId
+            };
+            return await _httpClient.GetWithRetry<ResultActivity>(url, Params);
+        }
+        catch (Exception err)
+        {
+            Logger.Error("Не удалось получить информацию о активности пользователя", err);
+            throw;
+        }
+    }
+
+    public async Task<ResultStars> GetUserStarsAsync(long UserId)
+    {
+        try
+        {
+            string url = $"{_baseIrisUrl}/{ApiConstants.UserStars}";
+            var Params = new Dictionary<string, object>
+            {
+                ["user_id"] = UserId
+            };
+            return await _httpClient.GetWithRetry<ResultStars>(url, Params);
+        }
+        catch (Exception err)
+        {
+            Logger.Error("Не удалось получить информацию о рейтинге пользователя", err);
+            throw;
+        }
+    }
+
+    public async Task<ResultBag> GetUserBagAsync(long UserId)
+    {
+        try
+        {
+            string url = $"{_baseIrisUrl}/{ApiConstants.UserBag}";
+            var Params = new Dictionary<string, object>
+            {
+                ["user_id"] = UserId
+            };
+            return await _httpClient.GetWithRetry<ResultBag>(url, Params);
+        }
+        catch (Exception err)
+        {
+            Logger.Error("Не удалось получить информацию о мешке пользователя", err);
+            throw;
         }
     }
 }
