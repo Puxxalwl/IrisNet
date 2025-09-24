aa62ef64a4006149722bd3a411f258da96b2c889
Puxxalwl <puxxalwl98@gmail.com>
2025-09-24 09:18:59 +0300

    Добавлены новые константы с новой версии 0.3

--- Iris/Constants/ApiConstants.cs
+++ Iris/Constants/ApiConstants.cs
@@ -2,7 +2,7 @@ namespace Iris.Constants;
 
 internal class ApiConstants
 {
-    public const string _versionIris = "0.2";
+    public const string _versionIris = "0.3";
     public const string GiveSweets = "pocket/sweets/give";
     public const string Balance = "pocket/balance";
     public const string SweetsHistory = "pocket/sweets/history";
@@ -13,7 +13,14 @@ internal class ApiConstants
     public const string AllowUser = "pocket/allow_user";
     public const string DenyUser = "pocket/deny_user";
     public const string AllAllow = "pocket/allow_all";
-    public const string AllDeny = "pocket/deny_all"; 
+    public const string DonateScoreGive = "pocket/donate_score/give";
+    public const string DonateScoreHistory = "pocket/donate_score/history";
+    public const string AllDeny = "pocket/deny_all";
     public const string GetUpdates = "get_updates";
     public const string IrisAgents = "iris_agents";
+    public const string UserReg = "user_info/reg";
+    public const string UserBag = "user_info/pocket";
+    public const string UserActivity = "user_info/activity";
+    public const string UserStars = "user_info/stars";
+    public const string UserSpam = "user_info/spam";
 }
